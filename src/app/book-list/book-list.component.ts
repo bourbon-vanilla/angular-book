2ebf7c65617f9952b2cb3513a1a350e023c7b94b
Raphael <sobczyk.raphael@siemens.com>
2022-02-22 17:54:38 +0100

    Implement dependency injection

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { BookStoreService } from '../shared/book-store.service';
 import { IBook } from '../shared/ibook';
 
 @Component({
@@ -8,30 +9,14 @@ import { IBook } from '../shared/ibook';
 })
 export class BookListComponent implements OnInit {
 
-  public books: IBook[]
+  books: IBook[] = [];
+
   @Output() showDetailsEvent = new EventEmitter<IBook>();
 
-  constructor() { 
-    this.books = []
-  }
+  constructor(private bookStore: BookStoreService) { }
 
   ngOnInit(): void {
-    this.books = [
-      {
-        isbn: '9783864906466', 
-        title: 'Angular', 
-        authors: ['Ferdinand Malcher', 'Johannes Hoppe', 'Danny Koppenhagen'], 
-        published: new Date(2019, 4, 30), 
-        subtitle: 'Grundlagen, fortgeschrittene Themen und Best Practices -mit NativeScript und NgRx', rating: 5, thumbnails: [{ url: 'https://ng-buch.de/buch1.jpg', title: 'Buchcover' }], description: 'Die Autoren führen Sie mit einem → anspruchsvollen Beispielprojekt durch die Welt von → Angular...'
-      },
-      {
-        isbn: '9783864903274', 
-        title: 'React', authors: ['Oliver Zeigermann', 'Nils Hartmann'], 
-        published: new Date(2016, 6, 17), 
-        subtitle: 'Die praktische Einführung in React, React Router und Redux', 
-        rating: 3, thumbnails: [{ url: 'https://ng-buch.de/buch2.jpg', title: 'Buchcover' }], description: 'React ist ein JavaScript-Framework zur → Entwicklung von Benutzeroberflächen...'
-      }
-    ];
+    this.books = this.bookStore.getAll();
   }
 
   showDetails(book: IBook){
